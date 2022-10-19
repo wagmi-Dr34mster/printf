147bc70dd3794acbc6f19e27993da5549bf4b4c9
Glory <oyenekanglory99@gmail.com>
2022-10-18 17:08:46 -0700

    printf

--- main.h
+++ main.h
@@ -1,9 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 #include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
 * struct print - structure for printing various types
